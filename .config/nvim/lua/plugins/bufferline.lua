9bb1d04ebec586090c38de12cb35d48b41b231d8
Vincent Wong <vincent@vwong.dev>
2023-01-26 21:09:43 +1100

    feat(nvim): simplify shortcut for closing current buffer

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,4 +2,4 @@ require('bufferline').setup()
 
 vim.keymap.set('n', '<leader>j', ':BufferLineCyclePrev<CR>')
 vim.keymap.set('n', '<leader>k', ':BufferLineCycleNext<CR>')
-vim.keymap.set('n', '<leader>w', ':BufferLinePickClose<CR>')
+vim.keymap.set('n', '<leader>x', ':bd<CR>')
